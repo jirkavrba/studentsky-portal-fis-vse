84a247c5aa2a733f671922cba6f2ba8493bcfe47
Jiří Vrba <jiri@vrba.dev>
2021-01-11 14:23:06 +0100

    Added authentication controller, removed tailwinds and added bootstrap instead

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
+
+  get '/sign_in', to: 'authentication#login'
+  get '/sign_up', to: 'authentication#registration'
+
+  post '/sign_in', to: 'authentication#process_login'
+  post '/sign_up', to: 'authentication#process_registration'
 end

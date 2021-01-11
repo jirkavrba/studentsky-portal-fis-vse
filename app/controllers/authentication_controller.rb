294373daad88d76e8d0c865aa7fe3d15ba3c4949
Jiří Vrba <jiri@vrba.dev>
2021-01-11 16:18:15 +0100

    Added user registration

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,36 @@
+# frozen_string_literal: true
+
 class AuthenticationController < ApplicationController
-  def login
+  def login; end
+
+  def registration
     @user = User.new
+  end
+
+  def process_login
+    login_params = params.permit(:username, :password)
+    user = User.find_by username: login_params[:username]
+
+    if user.nil? || !user.authenticate(login_params[:password])
+      return redirect_to sign_in_url,
+                         alert: 'Nesprávné přihlašovací údaje nebo uživatel neexistuje.'
+    end
+
+    session[:user_id] = user.id
+
+    redirect_to root_url, notice: "Přihlášen jako #{user.display_name}"
+  end
+
+  def process_registration
+    registration_params = params.require(:user).permit(:username, :name, :password)
+    user = User.new registration_params
+
+    if user.valid?
+      user.save!
+      # TODO: Send verification email to user.username@vse.cz
+      redirect_to sign_in_url, notice: "Na email #{user.username}@vse.cz byl odeslán aktivační odkaz."
+    else
+      redirect_to sign_up_url, alert: user.errors.full_messages
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[0-9a-z]+\z/ }
+  validates_length_of :password, minimum: 8, allow_blank: false
+
+  def display_name
+    name || username
+  end
 end

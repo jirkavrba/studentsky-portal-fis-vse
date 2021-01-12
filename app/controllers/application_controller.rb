1c2543d4d9493b81c124e54b705c39eeee55e9b8
Jiří Vrba <jiri@vrba.dev>
2021-01-12 14:04:50 +0100

    Moved authentication helpers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def signed_in?
+    !session[:user_id].nil?
+  end
+
+  def current_user
+    @current_user ||= User.find session[:user_id]
+  end
 end

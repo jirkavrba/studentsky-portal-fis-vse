a8c170929abb5345d19b856468d35c88591555fb
Jiří Vrba <jiri@vrba.dev>
2021-01-13 22:10:42 +0100

    Removed is_active, as tokens can be revoked by deleting the token

--- db/migrate/20210113204928_create_api_tokens.rb
+++ db/migrate/20210113204928_create_api_tokens.rb
@@ -3,7 +3,6 @@ class CreateApiTokens < ActiveRecord::Migration[6.1]
     create_table :api_tokens do |t|
       t.string :name, null: true
       t.string :value, unique: true
-      t.boolean :is_active, default: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_082025) do
+ActiveRecord::Schema.define(version: 2021_01_13_204928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_tokens", force: :cascade do |t|
+    t.string "name"
+    t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "discord_verifications", force: :cascade do |t|
     t.integer "user_id"

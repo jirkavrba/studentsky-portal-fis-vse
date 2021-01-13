449ffb7543a3836554e7e9d1a12705ad36f19c2c
Jiří Vrba <jiri@vrba.dev>
2021-01-13 01:51:34 +0100

    Added AccessDenied handler and bunch of HTTP helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
-class ApplicationController < ActionController::Base
-  helper_method :authenticated?, :current_user
+# frozen_string_literal: true
 
+class ApplicationController < ActionController::Base
+  # Authentication
   def authenticate!
     redirect_to sign_in_url unless authenticated?
   end
@@ -16,4 +17,21 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find session[:user_id] if authenticated?
   end
+
+  helper_method :authenticated?, :current_user
+
+  # Http helpers
+  def forbidden
+    render text: '403: HTTP_FORBIDDEN', status: :forbidden
+  end
+
+  def not_found
+    render text: '404: HTTP_NOT_FOUND', status: :not_found
+  end
+
+  def internal_server_error
+    render text: '500: INTERNAL_SERVER_ERROR', status: :internal_server_error
+  end
+
+  rescue_from CanCan::AccessDenied, with: :forbidden
 end

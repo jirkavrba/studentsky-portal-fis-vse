20deeb1ca74b2b8e35f24659d0b57b72171b71a2
Jiří Vrba <jiri@vrba.dev>
2021-01-18 17:18:37 +0100

    Migrated subjects ActiveRecord table

--- db/migrate/20210118161707_create_subjects.rb
+++ db/migrate/20210118161707_create_subjects.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateSubjects < ActiveRecord::Migration[6.1]
   def change
     create_table :subjects do |t|
-      t.string :code
+      t.string :code, unique: true
       t.string :name
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_134135) do
+ActiveRecord::Schema.define(version: 2021_01_18_161707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2021_01_18_134135) do
   create_table "email_verifications", force: :cascade do |t|
     t.integer "user_id"
     t.string "verification_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

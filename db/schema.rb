76c2640a8db79eb91a35130930428aedcfff7551
Jiří Vrba <jiri@vrba.dev>
2021-01-19 15:03:07 +0100

    Added password_reset_token to users, so a reset link can be generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_011734) do
+ActiveRecord::Schema.define(version: 2021_01_19_140041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_011734) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "avatar_discriminator", default: ""
+    t.string "password_reset_token"
   end
 
 end

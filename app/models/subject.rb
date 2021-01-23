0db9d446784a3ad7b3380a2747db7c8e5855459c
Jiří Vrba <jiri@vrba.dev>
2021-01-23 09:52:20 +0100

    Subject <-> Tester associations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Subject < ApplicationRecord
+  has_many :testers
+
   validates :name, :code, presence: true
   validates_uniqueness_of :code, scope: :id
 end
--- app/models/tester.rb
+++ app/models/tester.rb
@@ -1,3 +1,6 @@
 class Tester < ApplicationRecord
   belongs_to :user
+  belongs_to :subject
+
+  validates_presence_of :subject_id, :user_id, :is_public
 end

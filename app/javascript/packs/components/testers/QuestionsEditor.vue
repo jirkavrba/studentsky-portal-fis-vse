11b33441911d5ede145f26777753a366f66be5ac
Jiří Vrba <jiri@vrba.dev>
2021-01-23 11:48:29 +0100

    Questions editor fully implemented, create tester UI

--- app/javascript/packs/components/testers/QuestionsEditor.vue
+++ app/javascript/packs/components/testers/QuestionsEditor.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-    <input type="hidden" name="tester[questions]" :value="serializedQuestions">
-    <div class="ui styled fluid accordion">
-      <div v-for="(question, i) in this.questions" :key="i">
-        <div class="title">{{ question.text }}</div>
-        <div class="content">
+    <input type="hidden" :name="name" :value="serializedQuestions">
+    <div v-for="(question, i) in this.questions" :key="i" class="ui segment">
+      <div class="ui top left attached label">Otázka {{ i + 1 }}</div>
+      <div class="field">
+        <label>Text otázky</label>
+        <input type="text" v-model="questions[i].text">
+      </div>
+      <div class="extra content">
+        <div class="field">
+          <label>Odpovědi</label>
+          <div v-if="question.answers.length === 0" class="ui basic segment">
+            <span class="ui grey text">
+              Tato otázka zatím nemá žádné odpovědi
+            </span>
+          </div>
+          <div v-for="(answer, j) in question.answers" :key="j" class="field">
+            <div class="ui action input">
+              <div :class="'ui icon button' + (answer.correct ? ' green' : '')" @click="toggleAnswer(i, j)">
+                <i :class="'icon ' + (answer.correct ? 'check' : 'times')"></i>
+              </div>
+              <input type="text" v-model="questions[i].answers[j].text" placeholder="Text odpovědi">
+              <div :class="'ui icon button'" @click="removeAnswer(i, j)">
+                <i class="trash icon"></i>
+              </div>
+            </div>
+          </div>
+          <div class="ui tiny button" @click="addAnswerToQuestion(i)">Přidat odpověď</div>
+          <div class="ui tiny red button" @click="removeQuestion(i)">Smazat otázku</div>
         </div>
       </div>
+    </div>
+    <div>
+      <div class="ui green button" @click="addQuestion">Přidat otázku</div>
     </div>
   </div>
 </template>
@@ -14,13 +40,35 @@
 
 export default {
   name: 'QuestionsEditor',
-  props: ['source'],
+  props: ['source', 'name'],
   data: () => ({
     questions: [],
-    currentQuestion: null
   }),
-  mounted: function() {
-    this.questions = JSON.parse(this.source)
+  mounted: function () {
+    this.questions = JSON.parse(this.source);
+  },
+  methods: {
+    addQuestion: function () {
+      this.questions.push({
+        text: "",
+        answers: []
+      })
+    },
+    removeQuestion: function (i) {
+      this.questions.splice(i, 1)
+    },
+    toggleAnswer: function (i, j) {
+      this.questions[i].answers[j].correct = !this.questions[i].answers[j].correct
+    },
+    removeAnswer: function (i, j) {
+      this.questions[i].answers.splice(j, 1)
+    },
+    addAnswerToQuestion: function (i) {
+      this.questions[i].answers.push({
+        text: "",
+        correct: false
+      })
+    }
   },
   computed: {
     serializedQuestions: function () {
--- app/models/tester.rb
+++ app/models/tester.rb
@@ -2,5 +2,5 @@ class Tester < ApplicationRecord
   belongs_to :user
   belongs_to :subject
 
-  validates_presence_of :subject_id, :user_id, :is_public
+  validates_presence_of :subject_id, :user_id, :questions, :is_public
 end
